ea1a7664e16b7d718ceecaa224480e3540da3635
kg-nehal <73439904+kg-nehal@users.noreply.github.com>
2021-01-10 13:48:34 -0500

    Add files via upload

---
+++ rose.js
@@ -0,0 +1,9 @@
+class rose extends BaseClass{
+    constructor(x,y){
+        super(x,y,50,50);
+        this.image = loadImage("sprites/rose1.png");
+    }
+    display(){
+        super.display();
+    }
+};
